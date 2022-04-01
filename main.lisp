9725e994d261b6ee25724c5e704e28f3ba278846
Chris Matzenbach <chris.matzenbach@omnisci.com>
2022-04-01 14:54:34 -0500

    Small sandbox program to get started

---
+++ main.lisp
@@ -0,0 +1,14 @@
+;; main window
+(defvar window (make-instance 'gtk:gtk-window :type :toplevel :title "Bleep"))
+(defvar vbox (make-instance 'gtk:gtk-box :orientation :vertical
+                                         :spacing 25
+                                         :margin 25))
+
+(gtk:within-main-loop
+ ;; quit program when window closed
+ (gobject:g-signal-connect window "destroy" (lambda (widget)
+                                              (declare (ignore widget))
+                                              (gtk:leave-gtk-main)))
+ ;; display gui
+ (gtk:gtk-container-add window vbox)
+ (gtk:gtk-widget-show-all window))

a8c254c7003110dc8aef6569296d7d92ac1a0be6
Chris Matzenbach <chris.matzenbach@omnisci.com>
2022-04-06 05:28:45 -0500

    One more .thrift file

---
+++ completion_hints.thrift
@@ -0,0 +1,19 @@
+namespace java ai.heavy.thrift.calciteserver
+namespace py heavy.completion_hints
+
+enum TCompletionHintType {
+  COLUMN,
+  TABLE,
+  VIEW,
+  SCHEMA,
+  CATALOG,
+  REPOSITORY,
+  FUNCTION,
+  KEYWORD
+}
+
+struct TCompletionHint {
+  1: TCompletionHintType type;
+  2: list<string> hints;
+  3: string replaced;
+}

3989c0be0c38a5f47a6a7937bb1868078dd22b25
Chris Matzenbach <chris.matzenbach@omnisci.com>
2022-04-08 10:45:25 -0500

    Refactored data-middleware to use generic function to make sql queries

--- src/data-middleware.lisp
+++ src/data-middleware.lisp
@@ -1,7 +1,7 @@
 (in-package :data-middleware)
 
-(defun find-first-goalie ()
+(defun run-sql (sql)
   (dbi:with-connection (conn :postgres :database-name "lightdb" :host "127.0.0.1" :port 5432 :username "postgres" :password "postgres")
-    (let* ((query (dbi:prepare conn "SELECT * FROM hockey.goalies WHERE goalies_pk = 1"))
+    (let* ((query (dbi:prepare conn sql))
            (query (dbi:execute query)))
       (format nil "~a" (dbi:fetch query)))))

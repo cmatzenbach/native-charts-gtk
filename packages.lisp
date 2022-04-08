0bf083c8b04b3760010b41261605a2a49ef95956
Chris Matzenbach <chris.matzenbach@omnisci.com>
2022-04-08 14:00:11 -0500

    Export the run-sql function

--- packages.lisp
+++ packages.lisp
@@ -2,7 +2,8 @@
   (:use :gtk :gdk :gdk-pixbuf :gobject :glib :gio :pango :cairo :common-lisp))
 
 (defpackage data-middleware
-  (:use :dbi :cl))
+  (:use :dbi :cl)
+  (:export :run-sql))
 
 (defpackage bar-chart
   (:use :gtk :gdk :gdk-pixbuf :gobject :glib :gio :pango :cairo :common-lisp))
--- src/data-middleware.lisp
+++ src/data-middleware.lisp
@@ -4,4 +4,4 @@
   (dbi:with-connection (conn :postgres :database-name "lightdb" :host "127.0.0.1" :port 5432 :username "postgres" :password "postgres")
     (let* ((query (dbi:prepare conn sql))
            (query (dbi:execute query)))
-      (format nil "~a" (dbi:fetch query)))))
+      (dbi:fetch-all query))))
